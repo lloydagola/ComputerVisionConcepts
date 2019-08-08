ef8cc06269c6da9ed0cd978aab8d5bad3c676bcb
lloydagola <lloydagola@gmail.com>
2019-08-08 09:31:36 +0600

    Record the camera feed to a file

--- CameraFeed/app.py
+++ CameraFeed/app.py
@@ -3,16 +3,26 @@ import cv2
 
 camera_feed = cv2.cv2.VideoCapture(0)
 
+fourcc = cv2.cv2.VideoWriter_fourcc(*"XVID")
+output = cv2.cv2.VideoWriter("output.avi", fourcc, 24.0, (640, 480))
+
 while camera_feed.isOpened():
     frameIsAvailable, current_frame = camera_feed.read()
 
     if frameIsAvailable:
+
+        #record the camera feed
+        output.write(current_frame)
+
         grey_scale = cv2.cv2.cvtColor(current_frame, cv2.cv2.COLOR_BGR2GRAY)
         cv2.cv2.imshow("Camera feed", grey_scale)
 
         if cv2.cv2.waitKey(1) & 0xFF == 27:
             break
+    else:
+         break
 
 camera_feed.release()
+output.release()
 cv2.cv2.destroyAllWindows()
 
