74ba4a256f1db3515d1cfd0dcfe8c0f13ebc940a
lloydagola <lloydagola@gmail.com>
2019-08-08 08:48:16 +0600

    Read video from the webcam

---
+++ CameraFeed/app.py
@@ -0,0 +1,15 @@
+import cv2
+
+
+camera_feed = cv2.cv2.VideoCapture(0)
+
+while camera_feed.isOpened():
+    _, current_frame = camera_feed.read()
+    cv2.cv2.imshow("Camera feed", current_frame)
+
+    if cv2.cv2.waitKey(1) & 0xFF == 27:
+        break
+
+camera_feed.release()
+cv2.cv2.destroyAllWindows()
+
