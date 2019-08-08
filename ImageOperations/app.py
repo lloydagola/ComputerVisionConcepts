09e8cd66d397c4c39dcb7f66958c441bab571d18
lloydagola <lloydagola@gmail.com>
2019-08-08 10:29:46 +0600

    split the image into individual bgr channels

--- ImageOperations/app.py
+++ ImageOperations/app.py
@@ -6,6 +6,10 @@ print(picture.shape)    #returns a tuple of number of rows, columns and channels
 print(picture.size)     #returns total number of pixels accessed
 print(picture.dtype)    #returns Image datatype obtained
 
+
+blue, green, red = cv2.cv2.split(picture)
+print(b)
+
 cv2.cv2.imshow("Result", picture)
 
 if cv2.cv2.waitKey(0) & 0xFF == 27:
