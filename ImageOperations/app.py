185ab4ade87bbc13d739f424fda35bd288fe04b4
lloydagola <lloydagola@gmail.com>
2019-08-08 10:21:02 +0600

    print various properties of the image

--- ImageOperations/app.py
+++ ImageOperations/app.py
@@ -2,6 +2,10 @@ import cv2
 
 picture = cv2.cv2.imread("pexels.jpeg")
 
+print(picture.shape)    #returns a tuple of number of rows, columns and channels
+print(picture.size)     #returns total number of pixels accessed
+print(picture.dtype)    #returns Image datatype obtained
+
 cv2.cv2.imshow("Result", picture)
 
 if cv2.cv2.waitKey(0) & 0xFF == 27:
