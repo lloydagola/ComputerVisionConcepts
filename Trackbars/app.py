3156a23b1700f9dc8e706bdb10c8e6d86b69abb3
lloydagola <lloydagola@gmail.com>
2019-08-28 11:57:46 +0300

    added a grayscale trackbar to toggle between color and grayscale images

--- Trackbars/app.py
+++ Trackbars/app.py
@@ -4,34 +4,35 @@ import cv2
 def do_stuff(x):
     print(x)
 
-# Create a black image, a window
-img = np.zeros((300,512,3), np.uint8)
-#creates a named window
-cv2.cv2.namedWindow("image")
+# Create a black window_name, a window
+cv2.cv2.namedWindow('window_name')
 
-#bind the trackbar to our named window
-cv2.cv2.createTrackbar("BlueTrackbar", "image", 0, 255, do_stuff)
-cv2.cv2.createTrackbar("GreenTrackbar", "image", 0, 255, do_stuff)
-cv2.cv2.createTrackbar("RedTrackbar", "image", 0, 255, do_stuff)
-
-toggle_bgr = "0 : OFF\n 1 : ON"
-cv2.cv2.createTrackbar(toggle_bgr, "image", 0, 1, do_stuff)
+cv2.cv2.createTrackbar('trackbar_name', 'window_name', 10, 400, do_stuff)
+cv2.cv2.createTrackbar('toggle_grayscale', 'window_name', 0, 1, do_stuff)
+cv2.cv2.createTrackbar("BlueTrackbar", "window_name", 0, 255, do_stuff)
+cv2.cv2.createTrackbar("GreenTrackbar", "window_name", 0, 255, do_stuff)
+cv2.cv2.createTrackbar("RedTrackbar", "window_name", 0, 255, do_stuff)
 
 while(1):
-    cv2.cv2.imshow("image",img)
-   
+    img = cv2.cv2.imread('pexels.jpeg')
+
+    some_value = cv2.cv2.getTrackbarPos('trackbar_name', 'window_name')
+    toggle_grayscale = cv2.cv2.getTrackbarPos('toggle_grayscale', 'window_name')
+
+    blueValue = cv2.cv2.getTrackbarPos("BlueTrackbar", "window_name")
+    greenValue = cv2.cv2.getTrackbarPos("GreenTrackbar", "window_name")
+    redValue = cv2.cv2.getTrackbarPos("RedTrackbar", "window_name")
+    
+    cv2.cv2.putText(img, str(some_value), (50, 150), cv2.cv2.FONT_HERSHEY_SIMPLEX, 6, (0, 0, 255), 10)
+
     if cv2.cv2.waitKey(1) & 0xFF == 27:
         break
 
-    blueValue = cv2.cv2.getTrackbarPos("BlueTrackbar", "image")
-    greenValue = cv2.cv2.getTrackbarPos("GreenTrackbar", "image")
-    redValue = cv2.cv2.getTrackbarPos("RedTrackbar", "image")
-    switchValue = cv2.cv2.getTrackbarPos(toggle_bgr, "image")
-
-    if switchValue == 0:
-       img[:] = 0
+    if toggle_grayscale == 0:
+        img[:] = [blueValue, greenValue, redValue]
     else:
-       img[:] = [blueValue, greenValue, redValue]
+       img = cv2.cv2.cvtColor(img, cv2.cv2.COLOR_BGR2GRAY)
 
+    img = cv2.cv2.imshow('window_name',img)
 
 cv2.cv2.destroyAllWindows()
