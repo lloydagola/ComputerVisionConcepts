d3f69972a2568667bde74762186a7f1328e5f309
lloydagola <lloydagola@gmail.com>
2019-08-29 11:43:37 +0300

    Added trackbars for easy color detection

---
+++ HSVObjectDetection/app.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+def do_stuff():
+    pass
+
+cv2.cv2.namedWindow("Tracking")
+cv2.cv2.createTrackbar("HueFloor", "Tracking", 0, 255, do_stuff)
+cv2.cv2.createTrackbar("SaturationFloor", "Tracking", 0, 255, do_stuff)
+cv2.cv2.createTrackbar("ValueFloor", "Tracking", 0, 255, do_stuff)
+cv2.cv2.createTrackbar("HueCeiling", "Tracking", 255, 255, do_stuff)
+cv2.cv2.createTrackbar("SaturationCeiling", "Tracking", 255, 255, do_stuff)
+cv2.cv2.createTrackbar("ValueCeiling", "Tracking", 255, 255, do_stuff)
+
+while True:
+    frame = cv2.cv2.imread("colored_balls.jpg")
+
+    hsvImage = cv2.cv2.cvtColor(frame, cv2.cv2.COLOR_BGR2HSV)
+
+    hue_floor = cv2.cv2.getTrackbarPos("HueFloor", "Tracking") 
+    saturation_floor = cv2.cv2.getTrackbarPos("SaturationFloor", "Tracking")
+    value_floor = cv2.cv2.getTrackbarPos("ValueFloor", "Tracking")
+
+    hue_ceiling = cv2.cv2.getTrackbarPos("HueCeiling", "Tracking")
+    saturation_ceiling = cv2.cv2.getTrackbarPos("SaturationCeiling", "Tracking")
+    value_ceiling = cv2.cv2.getTrackbarPos("ValueCeiling", "Tracking")
+
+    color_floor = np.array([hue_floor, saturation_floor, value_floor])
+    color_ceiling = np.array([hue_ceiling, saturation_ceiling, value_ceiling])
+
+    mask = cv2.cv2.inRange(hsvImage, color_floor, color_ceiling)
+
+    result = cv2.cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.cv2.imshow("Colored balls", frame)
+    cv2.cv2.imshow("Mask", mask)
+    cv2.cv2.imshow("Result", result)
+
+    if cv2.cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cv2.cv2.destroyAllWindows()
