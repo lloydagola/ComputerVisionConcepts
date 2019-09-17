480f871d7aad151e054494be37cfe0cdc2e78b52
lloydagola <lloydagola@gmail.com>
2019-09-17 11:31:01 +0300

    added a screenshot of the running program

--- AdaptiveThresholding/app.py
+++ AdaptiveThresholding/app.py
@@ -8,8 +8,8 @@ adaptive_threshold_gaussian_result = cv2.cv2.adaptiveThreshold(original_image, 2
 
 cv2.cv2.imshow("original", original_image)
 cv2.cv2.imshow("regular_threshold", regular_threshold_result)
-cv2.cv2.imshow("adaptive_threshold", adaptive_threshold_result)
-cv2.cv2.imshow("adaptive_threshold_gausian", adaptive_threshold_gaussian_result)
+cv2.cv2.imshow("adaptive_threshold_mean_C", adaptive_threshold_result)
+cv2.cv2.imshow("adaptive_threshold_gausian_C", adaptive_threshold_gaussian_result)
 
 cv2.cv2.waitKey(0)
 cv2.cv2.destroyAllWindows()
