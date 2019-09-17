4aaecfe8061e4306233dd4766d701f688f9ba2cb
lloydagola <lloydagola@gmail.com>
2019-09-17 11:27:37 +0300

    implemented ADAPTIVE_THRESH_GAUSSIAN_C

--- AdaptiveThresholding/app.py
+++ AdaptiveThresholding/app.py
@@ -2,10 +2,14 @@ import cv2
 import numpy as np
 
 original_image = cv2.cv2.imread("sudoku.jpg", 0)
-_, threshold_result = cv2.cv2.threshold(original_image, 127, 255, cv2.cv2.THRESH_BINARY)
+_, regular_threshold_result = cv2.cv2.threshold(original_image, 127, 255, cv2.cv2.THRESH_BINARY)
+adaptive_threshold_result = cv2.cv2.adaptiveThreshold(original_image, 255, cv2.cv2.ADAPTIVE_THRESH_MEAN_C, cv2.cv2.THRESH_BINARY, 11, 2)
+adaptive_threshold_gaussian_result = cv2.cv2.adaptiveThreshold(original_image, 255, cv2.cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.cv2.THRESH_BINARY, 11, 2)
 
 cv2.cv2.imshow("original", original_image)
-cv2.cv2.imshow("threshold", threshold_result)
+cv2.cv2.imshow("regular_threshold", regular_threshold_result)
+cv2.cv2.imshow("adaptive_threshold", adaptive_threshold_result)
+cv2.cv2.imshow("adaptive_threshold_gausian", adaptive_threshold_gaussian_result)
 
 cv2.cv2.waitKey(0)
 cv2.cv2.destroyAllWindows()
