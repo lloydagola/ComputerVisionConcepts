3bfb73f037b4e738571bad3eb11b075459a3651d
lloydagola <lloydagola@gmail.com>
2019-09-17 11:48:51 +0300

    converting BGR image read using OpenCV to RGB format and displaying it using matplotlib

--- Matplotlib/app.py
+++ Matplotlib/app.py
@@ -4,8 +4,9 @@ from matplotlib import pyplot
 
 original_image = cv2.cv2.imread("pexels.jpeg")
 cv2.cv2.imshow("original image", original_image)
+rgb_image = cv2.cv2.cvtColor(original_image, cv2.cv2.COLOR_BGR2RGB)
 
-pyplot.imshow(original_image)
+pyplot.imshow(rgb_image)
 pyplot.show()
 
 cv2.cv2.waitKey(0)
