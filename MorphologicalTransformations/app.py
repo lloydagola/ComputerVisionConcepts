4a2f2b1f1ece30d8a6f84c4bf5d624c16595a930
lloydagola <lloydagola@gmail.com>
2019-09-18 11:45:06 +0300

    Implemented Opening Morphological Transformation

--- MorphologicalTransformations/app.py
+++ MorphologicalTransformations/app.py
@@ -5,15 +5,16 @@ from matplotlib import pyplot
 original_image = cv2.cv2.imread("colored_balls.jpg", cv2.cv2.IMREAD_GRAYSCALE)
 _, mask = cv2.cv2.threshold(original_image, 220, 255, cv2.cv2.THRESH_BINARY_INV)
 
-kernal = numpy.ones((5,5), numpy.uint8)
+kernal = numpy.ones((2,2), numpy.uint8)
 dilation = cv2.cv2.dilate(mask, kernal, iterations=2)
 erosion = cv2.cv2.erode(mask, kernal, iterations=1)
+opening = cv2.cv2.morphologyEx(mask, cv2.cv2.MORPH_OPEN, kernal)
 
-titles = ['image', 'mask', 'dilation', 'erosion']
-images = [original_image, mask, dilation, erosion]
+titles = ['image', 'mask', 'dilation', 'erosion', 'opening']
+images = [original_image, mask, dilation, erosion, opening]
 
-for i in range(4):
-    pyplot.subplot(2, 2, i+1), pyplot.imshow(images[i], 'gray')
+for i in range(5):
+    pyplot.subplot(2, 3, i+1), pyplot.imshow(images[i], 'gray')
     pyplot.title(titles[i])
     pyplot.xticks([]), pyplot.yticks([])
 
