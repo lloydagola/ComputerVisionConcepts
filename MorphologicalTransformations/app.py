45e49a21bde7289edb0deecc6d07d486e47e5a71
lloydagola <lloydagola@gmail.com>
2019-09-18 11:31:22 +0300

    Read and display an image

--- MorphologicalTransformations/app.py
+++ MorphologicalTransformations/app.py
@@ -1,3 +1,15 @@
 import cv2
 import numpy
 from matplotlib import pyplot
+
+original_image = cv2.cv2.imread("colored_balls.jpg", cv2.cv2.IMREAD_GRAYSCALE)
+
+titles = ['image']
+images = [original_image]
+
+for i in range(1):
+    pyplot.subplot(1, 1, i+1), pyplot.imshow(images[i], 'gray')
+    pyplot.title(titles[i])
+    pyplot.xticks([]), pyplot.yticks([])
+
+pyplot.show()
