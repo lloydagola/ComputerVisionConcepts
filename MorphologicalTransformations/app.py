ec3962bc748f310732b90285cc42c1ef910fd061
lloydagola <lloydagola@gmail.com>
2019-09-18 11:35:50 +0300

    Using dilation to reduce black spot imperfections within the mask

--- MorphologicalTransformations/app.py
+++ MorphologicalTransformations/app.py
@@ -5,11 +5,14 @@ from matplotlib import pyplot
 original_image = cv2.cv2.imread("colored_balls.jpg", cv2.cv2.IMREAD_GRAYSCALE)
 _, mask = cv2.cv2.threshold(original_image, 220, 255, cv2.cv2.THRESH_BINARY_INV)
 
-titles = ['image', 'mask']
-images = [original_image, mask]
+kernal = numpy.ones((2,2), numpy.uint8)
+dilation = cv2.cv2.dilate(mask, kernal)
 
-for i in range(2):
-    pyplot.subplot(1, 2, i+1), pyplot.imshow(images[i], 'gray')
+titles = ['image', 'mask', 'dilation']
+images = [original_image, mask, dilation]
+
+for i in range(3):
+    pyplot.subplot(1, 3, i+1), pyplot.imshow(images[i], 'gray')
     pyplot.title(titles[i])
     pyplot.xticks([]), pyplot.yticks([])
 
