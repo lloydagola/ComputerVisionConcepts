2e22bb74a33746ef4182ede7026a915f1beee30c
lloydagola <lloydagola@gmail.com>
2019-09-18 11:32:09 +0300

    Applying a mask to the original image

--- MorphologicalTransformations/app.py
+++ MorphologicalTransformations/app.py
@@ -3,12 +3,13 @@ import numpy
 from matplotlib import pyplot
 
 original_image = cv2.cv2.imread("colored_balls.jpg", cv2.cv2.IMREAD_GRAYSCALE)
+_, mask = cv2.cv2.threshold(original_image, 220, 255, cv2.cv2.THRESH_BINARY_INV)
 
-titles = ['image']
-images = [original_image]
+titles = ['image', 'mask']
+images = [original_image, mask]
 
-for i in range(1):
-    pyplot.subplot(1, 1, i+1), pyplot.imshow(images[i], 'gray')
+for i in range(2):
+    pyplot.subplot(1, 2, i+1), pyplot.imshow(images[i], 'gray')
     pyplot.title(titles[i])
     pyplot.xticks([]), pyplot.yticks([])
 
