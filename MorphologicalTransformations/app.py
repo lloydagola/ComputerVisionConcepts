07e35d9415fa41ae730c5cc1de709f060a4f409e
lloydagola <lloydagola@gmail.com>
2019-09-18 12:03:48 +0300

    Implemented morphological transformation on text

--- MorphologicalTransformations/app.py
+++ MorphologicalTransformations/app.py
@@ -2,21 +2,49 @@ import cv2
 import numpy
 from matplotlib import pyplot
 
-original_image = cv2.cv2.imread("colored_balls.jpg", cv2.cv2.IMREAD_GRAYSCALE)
-_, mask = cv2.cv2.threshold(original_image, 220, 255, cv2.cv2.THRESH_BINARY_INV)
 
-kernal = numpy.ones((2,2), numpy.uint8)
-dilation = cv2.cv2.dilate(mask, kernal, iterations=2)
-erosion = cv2.cv2.erode(mask, kernal, iterations=1)
-opening = cv2.cv2.morphologyEx(mask, cv2.cv2.MORPH_OPEN, kernal)
-closing = cv2.cv2.morphologyEx(mask, cv2.cv2.MORPH_CLOSE, kernal)
+def display_colored_balls():
+    original_image = cv2.cv2.imread("colored_balls.jpg", cv2.cv2.IMREAD_GRAYSCALE)
+    _, mask = cv2.cv2.threshold(original_image, 220, 255, cv2.cv2.THRESH_BINARY_INV)
 
-titles = ['image', 'mask', 'dilation', 'erosion', 'opening','closing']
-images = [original_image, mask, dilation, erosion, opening, closing]
+    kernal = numpy.ones((2,2), numpy.uint8)
+    dilation = cv2.cv2.dilate(mask, kernal, iterations=2)
+    erosion = cv2.cv2.erode(mask, kernal, iterations=1)
+    opening = cv2.cv2.morphologyEx(mask, cv2.cv2.MORPH_OPEN, kernal)
+    closing = cv2.cv2.morphologyEx(mask, cv2.cv2.MORPH_CLOSE, kernal)
+    morphologicalGradient = cv2.cv2.morphologyEx(mask, cv2.cv2.MORPH_GRADIENT, kernal)
+    tophat = cv2.cv2.morphologyEx(mask, cv2.cv2.MORPH_TOPHAT, kernal)
 
-for i in range(6):
-    pyplot.subplot(2, 3, i+1), pyplot.imshow(images[i], 'gray')
-    pyplot.title(titles[i])
-    pyplot.xticks([]), pyplot.yticks([])
+    titles = ['image', 'mask', 'dilation', 'erosion', 'opening','closing','morphologicalGradient', 'tophat']
+    images = [original_image, mask, dilation, erosion, opening, closing,morphologicalGradient, tophat]
 
-pyplot.show()
+    for i in range(8):
+        pyplot.subplot(3, 3, i+1), pyplot.imshow(images[i], 'gray')
+        pyplot.title(titles[i])
+        pyplot.xticks([]), pyplot.yticks([])
+
+    pyplot.show()
+
+def display_writing():
+    original_image = cv2.cv2.imread("text.png", cv2.cv2.IMREAD_GRAYSCALE)
+
+    kernal = numpy.ones((2,2), numpy.uint8)
+    dilation = cv2.cv2.dilate(original_image, kernal, iterations=2)
+    erosion = cv2.cv2.erode(original_image, kernal, iterations=1)
+    opening = cv2.cv2.morphologyEx(original_image, cv2.cv2.MORPH_OPEN, kernal)
+    closing = cv2.cv2.morphologyEx(original_image, cv2.cv2.MORPH_CLOSE, kernal)
+    morphologicalGradient = cv2.cv2.morphologyEx(original_image, cv2.cv2.MORPH_GRADIENT, kernal)
+    tophat = cv2.cv2.morphologyEx(original_image, cv2.cv2.MORPH_TOPHAT, kernal)
+
+    titles = ['image', 'dilation', 'erosion', 'opening','closing','morphologicalGradient', 'tophat']
+    images = [original_image, dilation, erosion, opening, closing,morphologicalGradient, tophat]
+
+    for i in range(7):
+        pyplot.subplot(3, 3, i+1), pyplot.imshow(images[i], 'gray')
+        pyplot.title(titles[i])
+        pyplot.xticks([]), pyplot.yticks([])
+
+    pyplot.show()
+
+display_colored_balls()
+display_writing()
