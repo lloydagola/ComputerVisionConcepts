8336ab4d25b3e698c7a6b32ab5663fb5e3999452
lloydagola <lloydagola@gmail.com>
2019-09-18 11:47:19 +0300

    Implemented Closing Morphological Transformation

--- MorphologicalTransformations/app.py
+++ MorphologicalTransformations/app.py
@@ -9,11 +9,12 @@ kernal = numpy.ones((2,2), numpy.uint8)
 dilation = cv2.cv2.dilate(mask, kernal, iterations=2)
 erosion = cv2.cv2.erode(mask, kernal, iterations=1)
 opening = cv2.cv2.morphologyEx(mask, cv2.cv2.MORPH_OPEN, kernal)
+closing = cv2.cv2.morphologyEx(mask, cv2.cv2.MORPH_CLOSE, kernal)
 
-titles = ['image', 'mask', 'dilation', 'erosion', 'opening']
-images = [original_image, mask, dilation, erosion, opening]
+titles = ['image', 'mask', 'dilation', 'erosion', 'opening','closing']
+images = [original_image, mask, dilation, erosion, opening, closing]
 
-for i in range(5):
+for i in range(6):
     pyplot.subplot(2, 3, i+1), pyplot.imshow(images[i], 'gray')
     pyplot.title(titles[i])
     pyplot.xticks([]), pyplot.yticks([])
